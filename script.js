6eb8ec2fec651650bc6b2c31dcc92e737d66e5bd
Urgazieva Aigerim <aigerim.urgazieva.on@gmail.com>
2024-01-26 19:30:58 +0600

    format script file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//book constructor: он будет престовлять книгу, поэтому каждый раз когда мы создаем книгу, она будет добавлять экземплярь объекта книги   
-function Book(title, author, isbn){
+//book constructor: он будет престовлять книгу, поэтому каждый раз когда мы создаем книгу, она будет добавлять экземплярь объекта книги
+function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
   this.isbn = isbn;
@@ -7,13 +7,13 @@ function Book(title, author, isbn){
 
 //UI constructor: будет обрабатывать задачи пользовательского интерфейса
 
-function UI(){}
+function UI() {}
 
 //UI prototype method
 
 UI.prototype.addBookToList = function (book) {
-  const booklist = document.getElementById('book-list');
-  const row = document.createElement('tr');
+  const booklist = document.getElementById("book-list");
+  const row = document.createElement("tr");
 
   row.innerHTML = `
   <td> ${book.title} </td>
@@ -22,46 +22,43 @@ UI.prototype.addBookToList = function (book) {
    <td><a href="#" class="delete">X</a></td>
    `;
 
-   booklist.appendChild(row);
-
+  booklist.appendChild(row);
 };
 
 //метод который будет покаывать alert
 //function будет принимать на себя два параметра первое сообщение которое будет выводится и исходя от типа сообщения будем передовать второе значение через имени класса (тоесть error или success )
 
-UI.prototype.showAlert = function(msg, className) {
-  const div = document.createElement('div');
+UI.prototype.showAlert = function (msg, className) {
+  const div = document.createElement("div");
   div.className = `alert ${className}`;
 
-//в div пепедаем apendchild-дом и в Аппедн чайл мы передаем текстовый узел к которому передаем аргумент msg
+  //в div пепедаем apendchild-дом и в Аппедн чайл мы передаем текстовый узел к которому передаем аргумент msg
 
   div.appendChild(document.createTextNode(msg));
 
-//дальше мы этот алерт должны вставить в html между контейнером и формой и для этого мы будем испоользовать insert before, должны были использовать insert before но так как у меня вертска с бутстрапом, потому что между родителем и нужного нам тега много элементов, и поэтому используем after
+  //дальше мы этот алерт должны вставить в html между контейнером и формой и для этого мы будем испоользовать insert before, должны были использовать insert before но так как у меня вертска с бутстрапом, потому что между родителем и нужного нам тега много элементов, и поэтому используем after
 
-const h2 = document.querySelector('h2');
-h2.after(div);
+  const h2 = document.querySelector("h2");
+  h2.after(div);
 
-// У сет таймаута два параметра первый нужно передать коллбек функцию и второе время 
-
-setTimeout(function(){
-document.querySelector('.alert').remove()
-},3000);
-
+  // У сет таймаута два параметра первый нужно передать коллбек функцию и второе время
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
 };
 
-//clear field 
-UI.prototype.clearFields = function(){
-  document.getElementById('name').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
+//clear field
+UI.prototype.clearFields = function () {
+  document.getElementById("name").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
 };
 
 //Delete book list method
 
-UI.prototype.deleteBookList = function(e) {
-  if (e.className === 'delete'){
-    e.parentElement.parentElement.remove()
+UI.prototype.deleteBookList = function (e) {
+  if (e.className === "delete") {
+    e.parentElement.parentElement.remove();
   }
 };
 
@@ -70,32 +67,29 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
   const title = document.getElementById("name").value,
     author = document.getElementById("author").value,
     isbn = document.getElementById("isbn").value;
-  
+
   const book = new Book(title, author, isbn);
- 
-   const ui = new UI();
 
+  const ui = new UI();
 
-//validate
-  if(title === '' || author === '' || isbn === ''){
-    ui.showAlert('please ', 'error');
-   } else if(isNaN(isbn)) {
-    ui.showAlert('incorrect ISBN number', 'error');
+  //validate
+  if (title === "" || author === "" || isbn === "") {
+    ui.showAlert("please ", "error");
+  } else if (isNaN(isbn)) {
+    ui.showAlert("incorrect ISBN number", "error");
   } else {
     ui.addBookToList(book);
-    ui.showAlert('ok', 'success');
+    ui.showAlert("ok", "success");
     ui.clearFields();
   }
 
   e.preventDefault();
-
 });
 
-// event listener delete book list 
-document.getElementById('book-list').addEventListener('click',(e) => {
-  
+// event listener delete book list
+document.getElementById("book-list").addEventListener("click", (e) => {
   const ui = new UI();
   ui.deleteBookList(e.target);
-  
-  e.preventDefault
+
+  e.preventDefault;
 });
